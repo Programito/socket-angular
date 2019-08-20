b01993afdc441c1bdb6aec082b2944c72111cf67
Programito <Programito@gmail.com>
2019-08-20 17:30:01 +0200

    clase usuario

--- src/app/services/websocket.service.ts
+++ src/app/services/websocket.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Socket } from 'ngx-socket-io';
+import { Usuario } from '../classes/usuario';
 
 @Injectable({
   providedIn: 'root'
@@ -7,6 +8,7 @@ import { Socket } from 'ngx-socket-io';
 export class WebsocketService {
 
   public socketStatus = false;
+  public usuario: Usuario; 
 
   constructor(
     private socket: Socket
@@ -28,12 +30,25 @@ export class WebsocketService {
   }
 
   emit(evento: string, payload?: any, callback?: Function) {
-    console.log('Emitiendo ', evento);
+    // console.log('Emitiendo ', evento);
     this.socket.emit(evento, payload, callback);
   }
 
   listen(evento: string) {
     return this.socket.fromEvent(evento);
+  }
+
+  loginWS(nombre: string) {
+    console.log('Configurando', nombre);
+
+    this.emit('configurar-usuario', {nombre}, resp => {
+      console.log(resp);
+    });
+    
+    // this.socket.emit('configurar-usuario', {nombre}, (resp) => {
+    //    console.log(resp);
+    // });
+
   }
 
 
